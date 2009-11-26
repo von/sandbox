ae7439720740448e43d44ddf7acb4bfab625beba
Von Welch <von@vwelch.com>
2009-11-25 20:03:25 -0600

    Added teatHashArg()

--- perl/varargs.pl
+++ perl/varargs.pl
@@ -1,6 +1,16 @@
 #!/usr/bin/env perl
 
+print "testFunc:\n";
 testFunc("hello world", -world=>"Earth", -lifetime=>100, -bar=>"foo");
+
+print "testHashArg:\n";
+my %hash = (
+    "foo" => "bar",
+    "one" => 1,
+    "file" => "name"
+    );
+
+testHashArg(%hash);
 
 sub testFunc
 {
@@ -10,5 +20,14 @@ sub testFunc
     foreach my $key (keys(%args))
     {
 	print $key . ":" . $args{$key} . "\n";
+    }
+}
+
+sub testHashArg
+{
+    my @args = @_;
+    foreach my $arg (@args)
+    {
+	print "Arg: $arg\n";
     }
 }
