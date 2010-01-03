9326663338214526f41e1c3cf0f46f2055d7256b
Von Welch <von@vwelch.com>
2010-01-03 14:14:41 -0600

    Added comments.

--- perl/strings.pl
+++ perl/strings.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/env perl
 
 my $s1 = 'Hello "World"';
-my $s2 = '$s1 and So Long...';
-my $s3 = "$s1 and 'so long'...";
+my $s2 = '$s1 and So Long...';  # $s1 will be taken literally
+my $s3 = "$s1 and 'so long'..."; # $s1 will be expanded
 
 print $s1 . "\n";
 print $s2 . "\n";
