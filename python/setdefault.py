dac2f4e07eaa8e41d34f686881d0338068be99ed
Von Welch <von@vwelch.com>
2010-02-10 07:55:01 -0600

    Added setdefault() example from http://www.saltycrane.com/blog/2010/02/python-setdefault-example/

---
+++ python/setdefault.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# Kudos:
+# http://www.saltycrane.com/blog/2010/02/python-setdefault-example/
+
+DATA_SOURCE = (('key1', 'value1'),
+               ('key1', 'value2'),
+               ('key2', 'value3'),
+               ('key2', 'value4'),
+               ('key2', 'value5'),)
+
+newdata = {}
+for k, v in DATA_SOURCE:
+    if newdata.has_key(k):
+        newdata[k].append(v)
+    else:
+        newdata[k] = [v]
+print newdata
+
+# This will return same as above.
+
+newdata = {}
+for k, v in DATA_SOURCE:
+    newdata.setdefault(k, []).append(v)
+print newdata
