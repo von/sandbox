7cb46f7285b02606db78a4afd5ad0408ee786680
Von Welch <von@vwelch.com>
2010-02-25 09:41:20 -0600

    Added fdel and doc arguments to property() call.

--- python/property.py
+++ python/property.py
@@ -5,9 +5,12 @@ Demonstrate use of property()
 Kudos:
 http://adam.gomaa.us/blog/2008/aug/11/the-python-property-builtin/
 """
+from pydoc import help
 import sys
 
 class Foo:
+    """Property test"""
+
     def __init__(self, value):
         self.value = value
 
@@ -21,8 +24,11 @@ class Foo:
     def setValue(self, value):
         self.value = value
 
+    def delValue(self):
+        del(self.value)
+
     # Define everything at once
-    value3 = property(getValue, setValue)
+    value3 = property(getValue, setValue, delValue, "value3 doc string")
 
 def main():
     f = Foo(4)
@@ -32,6 +38,8 @@ def main():
     assert(f.value3 == 4)
     f.value3 = 6
     assert(f.value3 == 6)
+    print help(Foo)
+    del(f.value3)
     print "Success."
     return 0
 
