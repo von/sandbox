4fd3c64d0b972e3a2cc91ae8e279f151e4f69ced
Von Welch <von@vwelch.com>
2010-07-08 21:48:39 -0500

    Added comment regarding "super() argument 1 must be type, not classobj" error.

--- python/super.py
+++ python/super.py
@@ -10,6 +10,12 @@ class A(object):
 class B(A):
     def routine(self):
         print "B.routine()"
+        # Note that the following error:
+        # "super() argument 1 must be type, not classobj"
+        # Means the super class is a classic class (i.e. not rooted at
+        # object) and super won't work. You have to use the class name
+        # explicitly.
+        # Kudos: http://www.velocityreviews.com/forums/t338909-help-with-super.html
         super(B, self).routine()
 
 if __name__ == '__main__':
