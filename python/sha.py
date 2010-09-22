1fd5881891c168e16306221fcefab9189d72e3d7
Von Welch <von@vwelch.com>
2010-09-21 20:41:04 -0500

    Added example use of SHA hashing.

---
+++ python/sha.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""An example python hashing application.
+
+%prog <file to hash>
+"""
+import hashlib
+from optparse import OptionParser
+import sys
+
+BLOCK_SIZE=1024
+
+def main(argv=None):
+    # Do argv default this way, as doing it in the functional
+    # declaration sets it at compile time.
+    if argv is None:
+        argv = sys.argv
+    parser = OptionParser(
+        usage=__doc__, # printed with -h/--help
+        version="%prog 1.0" # automatically generates --version
+        )
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        parser.error("Missing filename")
+    for filename in args:
+        hash = hashlib.sha1()
+        with open(filename) as f:
+            while True:
+                data = f.read(BLOCK_SIZE)
+                if not data:
+                    break
+                hash.update(data)
+            print "%s: %s" % (filename, hash.hexdigest())
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
