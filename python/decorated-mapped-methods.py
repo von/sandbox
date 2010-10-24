bedae3134328297291f667565ef571a19f424224
Von Welch <von@vwelch.com>
2010-10-24 10:19:42 -0500

    Use decorators to create function mappings.
    Kudos: http://www.scribd.com/doc/39946630/Python-Idioms

---
+++ python/decorated-mapped-methods.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""Demonstrate use of decorators to map methods to functions and methods.
+
+Kudos: http://www.scribd.com/doc/39946630/Python-Idioms (slides 9-10)
+Example does this with classes, this does it with functions.
+"""
+
+class Mapper(object):
+    mappings = {}
+
+    @classmethod
+    def register(cls, *names):
+        """Create a mapping between decorated function and given names"""
+        def decorator(function):
+            for name in names:
+                cls.mappings[name] = function
+            return function
+        return decorator
+
+    @classmethod
+    def get_by_name(cls, name):
+        """Return function associated with given name"""
+        return cls.mappings.get(name)
+
+    @classmethod
+    def invoke_by_name(cls, name, *args, **kw):
+        """Invoke the gunction associated with given name with given arguments"""
+        cls.mappings.get(name)(*args, **kw)
+
+@Mapper.register("foo", "bar")
+def do_foo(*args, **kw):
+    print "do_foo() called."
+    print "Args: " + " ".join(args)
+
+Mapper.get_by_name("foo")()
+Mapper.invoke_by_name("bar", "fie", "foe", "fum")
+
