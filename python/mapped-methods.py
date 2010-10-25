ff47aea8647f019ca5c05f5c8b8ee2523c7482de
Von Welch <von@vwelch.com>
2010-10-24 21:15:56 -0500

    Show how to call method based on a string mapping

---
+++ python/mapped-methods.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Show how to call method based on a string mapping
+
+See decorated-mapped-methods.py for a way to do this with decorators"""
+
+class A:
+    def __init__(self, value):
+        self.MAPPINGS[value](self)
+
+    def foo(self):
+        print "foo"
+
+    def bar(self):
+        print "bar"
+
+    def foobar(self):
+        print "foobar"
+
+    MAPPINGS = {
+        1 : foo,
+        2 : bar,
+        3 : foobar,
+        }
+
+A(1)
+A(2)
+A(3)
+
