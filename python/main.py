8b824ba0bd00cc2c9764701207986491373ba608
Von Welch <von@vwelch.com>
2010-10-30 11:32:23 -0500

    Updated to use argparse and format.

--- python/main.py
+++ python/main.py
@@ -7,31 +7,42 @@ This script docstring serves as a usage message (with -h).
 
 Kudos: http://www.artima.com/weblogs/viewpost.jsp?thread=4829
 
-Modifed to use optparse instead of getopt.
+Modifed to use argparse (new in 2.7) instead of getopt.
 """
 import sys
-from optparse import OptionParser
+import argparse
+
+def process(arg):
+    """Print the given argument"""
+    print "Argument: {}".format(arg)
+    if arg == "error":
+        return True
+    return False
 
 def main(argv=None):
     # Do argv default this way, as doing it in the functional
     # declaration sets it at compile time.
     if argv is None:
         argv = sys.argv
-    parser = OptionParser(
-        usage=__doc__, # printed with -h/--help
-        version="%prog 1.0" # automatically generates --version
+    parser = argparse.ArgumentParser(
+        description=__doc__, # printed with -h/--help
+        # Don't mess woth formation of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
         )
-    parser.add_option("-q", "--quiet", action="store_true", dest="quiet",
-                      help="run quietly", default=False)
-    (options, args) = parser.parse_args()
-    if options.quiet:
+    parser.add_argument("-q", "--quiet", action="store_true", dest="quiet",
+                        help="run quietly", default=False)
+    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument('args', metavar='args', type=str, nargs='+',
+                        help='some extra arguments')
+    args = parser.parse_args()
+    if args.quiet:
         print "Running quietly..."
     # process arguments.
-    for arg in args:
+    for arg in args.args:
         error = process(arg) # process() is defined elsewhere
         if error:
             # Example of using error() method, which exits
-            parser.error("Bad argument %s" % arg)
+            parser.error("Bad argument \"{}\"".format(arg))
     # Rest of main() function follows...
 
 if __name__ == "__main__":
