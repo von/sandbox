49a10f77d207b91703eaeca6e7c7b09c7bc55db9
Von Welch <von@vwelch.com>
2010-11-13 15:22:05 -0600

    Added more examples.

--- python/coroutine.py
+++ python/coroutine.py
@@ -19,6 +19,41 @@ def coroutine(func):
         return cr
     return start
 
+def generator_function(n):
+    """Return string representations of number 1..n"""
+    for i in range(n):
+        yield str(i)
+
+class generator_class(object):
+    def __init__(self, max):
+        self.count = 0
+        self.max = max
+
+    def __iter__(self):
+        """Called when instance appears in the for look. If you create an
+        instance first you can pass a value to __iter__(), e.g.:
+
+        gc = generator_class(10)
+        for i in gc(5):  # <- This requires __iter__ to accept a 2nd argument.
+            do_something(i)
+
+        If you use the class in the for line, __iter__ is called with no
+        arguments, e.g.:
+
+        # 10 gets passed to __init__()
+        # __iter__() just gets self
+        for i in generator_class(10): 
+            do_something(i)
+        """
+        return self
+
+    def next(self):
+        self.count += 1
+        if self.count > self.max:
+            raise StopIteration
+        else:
+            return str(self.count)
+
 @coroutine
 def grep(pattern, target):
     """A coroutine that looks for pattern in its input (which must be a string)
@@ -46,9 +81,20 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv
     print "Sending my source code to grep('while') and then printer()..."
-    # Take advantage of file being a generator here
+    # Take one with file as generator
     with file(argv[0]) as f:
         g = send_generator_to_coroutine(f, grep("while", printer("My output:")))
+    # Take two, building pipe slowly and using generator function
+    p = printer("Second output:")
+    g = grep("5", p)
+    gf = generator_function(10)
+    send_generator_to_coroutine(gf, g)
+    # Take three, with generator_class
+    p = printer("Third output:")
+    g = grep("7", p)
+    gc = generator_class(11)
+    send_generator_to_coroutine(gc, g)
+
     print "Done."
 
 if __name__ == "__main__":
