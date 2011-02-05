59da6a1f7118aa8639f562937931cd5bc1df77a9
Von Welch <von@vwelch.com>
2011-02-04 20:24:09 -0500

    Added example of unittests

---
+++ python/unittesting.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""Example of unittest module."""
+
+import unittest
+
+class TestSomething(unittest.TestCase):
+    """Tests for Something"""
+
+    def setUp(self):
+        """Called before each test method is run."""
+        pass
+
+    def test_something(self):
+        """A unittest"""
+        self.assertEqual(1, 1)
+
+    def test_something_else(self):
+        """Another unittest"""
+        self.assertEqual(0, 0)
+
+    def tearDown(self):
+        """Called after each test method is run."""
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
