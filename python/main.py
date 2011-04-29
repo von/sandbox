e3202ba3bb2f015645c5198cb075b95c92c472ca
Von Welch <von@vwelch.com>
2011-04-28 21:55:53 -0400

    Added use of argparse.parse_known_args() and ConfigParser to read defaults from a configuration file. Created parse_args() to pull out argument parsing from main()

--- python/main.py
+++ python/main.py
@@ -7,8 +7,10 @@ Kudos: http://www.artima.com/weblogs/viewpost.jsp?thread=4829
 
 Modified to use argparse (new in 2.7) instead of getopt.
 Modified to using logging for output.
+Modified to read configuration file for defaults.
 """
 import argparse
+import ConfigParser
 import logging
 import sys
 
@@ -19,6 +21,69 @@ def process(arg):
     if arg == "error":
         return True
     return False
+
+def parse_args(argv):
+    """Parse commandline arguments taking default from configuration file.
+
+    If --conf_file is specified, it will be read and used to set defaults
+    according to conf_mappings."""
+    # Parse any conf_file specification
+    # We make this parser with add_help=False so that
+    # it doesn't parse -h and print help.
+    conf_parser = argparse.ArgumentParser(
+        # Turn off help, so we print all options in response to -h
+        add_help=False
+        )
+    conf_parser.add_argument("-c", "--conf_file",
+                        help="Specify config file", metavar="FILE")
+    args, remaining_argv = conf_parser.parse_known_args()
+    defaults = {
+        "output_level" : logging.INFO,
+        "option" : "default option",
+        }
+    if args.conf_file:
+        # Mappings from configuraition file to options
+        conf_mappings = [
+            # ((section, option), option)
+            (("Defaults", "option"), "option")
+            ]
+        config = ConfigParser.SafeConfigParser()
+        config.read([args.conf_file])
+        for sec_opt, option in conf_mappings:
+            if config.has_option(*sec_opt):
+                value = config.get(*sec_opt)
+                defaults[option] = value
+
+    # Parse rest of arguments
+    # Don't surpress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser],
+        # print script description with -h/--help
+        description=__doc__,
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        )
+    parser.set_defaults(**defaults)
+    # Only allow one of debug/quiet mode
+    verbosity_group = parser.add_mutually_exclusive_group()
+    verbosity_group.add_argument("-d", "--debug",
+                                 action='store_const', const=logging.DEBUG,
+                                 dest="output_level", 
+                                 help="print debugging")
+    verbosity_group.add_argument("-q", "--quiet",
+                                 action="store_const", const=logging.WARNING,
+                                 dest="output_level",
+                                 help="run quietly")
+    parser.add_argument("-f", "--log_file",
+                        help="Log output to file", metavar="FILE")
+    parser.add_argument("--option", help="some option")
+    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument('args', metavar='args', type=str, nargs='+',
+                        help='some extra arguments' +\
+                            ' (use "error" to trigger an error)')
+    args = parser.parse_args(remaining_argv)
+    return args
 
 def main(argv=None):
     # Do argv default this way, as doing it in the functional
@@ -34,37 +99,15 @@ def main(argv=None):
     output_handler.setFormatter(logging.Formatter("%(message)s"))
     output.addHandler(output_handler)
 
-    # Argument parsing
-    parser = argparse.ArgumentParser(
-        description=__doc__, # printed with -h/--help
-        # Don't mess with format of description
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        # To have --help print defaults with trade-off it changes
-        # formatting, use: ArgumentDefaultsHelpFormatter
-        )
-    # Only allow one of debug/quiet mode
-    verbosity_group = parser.add_mutually_exclusive_group()
-    verbosity_group.add_argument("-d", "--debug",
-                                 action='store_const', const=logging.DEBUG,
-                                 dest="output_level", default=logging.INFO,
-                                 help="print debugging")
-    verbosity_group.add_argument("-q", "--quiet",
-                                 action="store_const", const=logging.WARNING,
-                                 dest="output_level",
-                                 help="run quietly")
-    parser.add_argument("-f", "--log_file",
-                        help="Log output to file", metavar="FILE")
-    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
-    parser.add_argument('args', metavar='args', type=str, nargs='+',
-                        help='some extra arguments' +\
-                            ' (use "error" to trigger an error)')
-    args = parser.parse_args()
+    args = parse_args(argv)
+
     output_handler.setLevel(args.output_level)
     if args.log_file:
         file_handler = logging.FileHandler(args.log_file)
         file_handler.setFormatter(logging.Formatter("%(asctime)s:%(message)s"))
         output.addHandler(file_handler)
         output.debug("Logging to file {}".format(args.log_file))
+    output.info("Option is \"{}\"".format(args.option))
     output.info("Processing arguments...")
     for arg in args.args:
         output.debug("Processing '{}'...".format(arg))
