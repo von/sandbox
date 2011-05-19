9428e51234a8b98d6b8d6b4fb6094b30c23f7f76
Von Welch <von@vwelch.com>
2011-05-19 14:52:32 -0400

    Add example of singleton in python.

---
+++ python/singleton.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""Demonstrate singleton in python.
+
+Kudos: http://code.activestate.com/recipes/66531/#c22
+
+TODO: Thread safety, add lock to __new__()
+
+Note good point in http://code.activestate.com/recipes/66531/#c23 that
+modules and classes are already singletons in python.
+"""
+
+class Singleton(object):
+    def __new__(cls, *p, **k):
+        if not '_the_instance' in cls.__dict__:
+            cls._the_instance = object.__new__(cls, *p, **k)
+        return cls._the_instance
+
+a = Singleton()
+b = Singleton()
+print a
+print b
+print a==b
