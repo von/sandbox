fb6261ff81c4fd89ce640f4e6337507f806968a0
Von Welch <von@vwelch.com>
2012-03-13 22:29:06 -0400

    Demonstration of 'import as'

---
+++ python/import-as.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Demonstrate 'import as'"""
+import re as regex
+from ConfigParser import SafeConfigParser as parser
+
+print "regex:", regex.__file__
+print "parser:", parser()
+
