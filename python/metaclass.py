c6f483e1e44c5c50637c3224cf5451d632133e40
Von Welch <von@vwelch.com>
2012-04-16 20:37:57 -0400

    Elaborate on use of metaclass.__call__() to intercept instance creation.

--- python/metaclass.py
+++ python/metaclass.py
@@ -5,29 +5,65 @@ Playing with Meta-classes.
 Basically, my words, a meta-class is what is used to create a class.
 Normally it is type.
 
-Kudos: http://www.voidspace.org.uk/python/articles/metaclasses.shtml
+Kudos:
+http://www.voidspace.org.uk/python/articles/metaclasses.shtml
+http://eli.thegreenplace.net/2012/04/16/python-object-creation-sequence/
 """
 
 from types import FunctionType
 
-class MetaClass(type):
+class ClassWithoutMeta(object):
+    """Class with no metaclass"""
+    
+    def __new__(cls, *args, **kwargs):
+        obj = super(ClassWithoutMeta, cls).__new__(cls)
+        print('ClassWithoutMeta.__new__ called. got new obj id=0x%x' % id(obj))
+        return obj
+
+    def __init__(self, arg):
+        print('ClassWithoutMeta.__init__ called (self=0x%x) with arg=%s' % (id(self), arg))
+        self.arg = arg
+
+print "type(ClassWithoutMeta)=", type(ClassWithoutMeta)
+t = ClassWithoutMeta("Hello World")
+print
+
+######################################################################
+
+
+class TestMetaClass(type):
+
     def __new__(meta, classname, bases, classDict):
-        print 'Creating new class: {}'.format(classname)
-        return type.__new__(meta, classname, bases, classDict)
+        """Called to construct the TestWithMetaClass class itself"""
+        obj = super(TestMetaClass, meta).__new__(meta, classname, bases, classDict)
+        print('TestMetaClass.__new__ called. got new obj id=0x%x' % id(obj))
+        return obj
 
-class Test(object):
-    """Just creating this class will case MetaClass.__new__() to be invoked
-    and "Creating new class: Test" to be printed."""
+    def __call__(cls, *args, **kwargs):
+        """Called with TestWithMetaClass() called since it is of type TestMetaClass
 
-    __metaclass__ = MetaClass
+        cls will be TestWithMetaClass"""
+        print('TestMetaClass.__call__()')
+        return cls.__new__(cls, *args, **kwargs)
+    
+class TestWithMetaClass(object):
+
+    __metaclass__ = TestMetaClass
+    
+    def __new__(cls, *args, **kwargs):
+        obj = super(TestWithMetaClass, cls).__new__(cls)
+        print('TestWithMetaClass.__new__ called. got new obj id=0x%x' % id(obj))
+        return obj
 
     def __init__(self):
-        pass
+        print('TestWithMetaClass.__init__ called (self=0x%x) with arg=%s' % (id(self), arg))
 
-    def method(self):
-        pass
-
-    classAttribute = 'Something'
+# type(TestWithMetaClass) is now TestMetaClass, so TestWithMetaClass()
+# invokes TestWithMetaClass.__call__()
+print "type(TestWithMetaClass)=", type(TestWithMetaClass)
+t = TestWithMetaClass()
+print "type(t)=", type(t)
+print
 
 ######################################################################
 
