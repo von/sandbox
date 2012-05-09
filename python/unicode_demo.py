b38790e7a27fd609ec832094772507c255f1e308
Von Welch <von@vwelch.com>
2012-05-09 16:03:04 -0400

    Added unicode_demo.py

---
+++ python/unicode_demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+"""Demonstrate Unicode
+
+Kudos: http://farmdev.com/talks/unicode/"""
+
+# Unicode is a set of bytes 
+u = u'Ivan Krsti\u0107'
+
+# Unicode gets encoded to strings
+s = u.encode('utf-8')
+
+print s
+
+print "Implicit conversion:", u
+
+# And strings decode into unicode
+u2 = s.decode('utf-8')
+
+print "u == u2?", u == u2
+
+print """
+Proposed solution by http://farmdev.com/talks/unicode/:
+    Decode early
+    Unicode everywhere
+    Encode late
+"""
+
