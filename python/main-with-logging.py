137c57fd4260912b8c03efba07cee8775b39447d
Von Welch <von@vwelch.com>
2012-07-21 15:01:27 -0400

    Simplified main.py, main-with-logginer.py has use of logging

--- python/main-with-logging.py
+++ python/main-with-logging.py
@@ -6,19 +6,15 @@ This script docstring serves as a usage message (with -h).
 Kudos: http://www.artima.com/weblogs/viewpost.jsp?thread=4829
 
 Modified to use argparse (new in 2.7) instead of getopt.
+Modified to using logging for output.
 """
-from __future__ import print_function  # So we can get at print()
-
 import argparse
+import logging
 import sys
-
-# Output functions
-output = print
-debug = print
 
 def process(arg):
     """Print the given argument"""
-    output("Argument: {}".format(arg))
+    print "Argument: {}".format(arg)
     # Allow command-line argument to create an error
     if arg == "error":
         return True
@@ -29,6 +25,14 @@ def main(argv=None):
     # declaration sets it at compile time.
     if argv is None:
         argv = sys.argv
+
+    # Set up out output via logging module
+    output = logging.getLogger(argv[0])
+    output.setLevel(logging.DEBUG)
+    output_handler = logging.StreamHandler(sys.stdout)  # Default is sys.stderr
+    # Set up formatter to just print message without preamble
+    output_handler.setFormatter(logging.Formatter("%(message)s"))
+    output.addHandler(output_handler)
 
     # Argument parsing
     parser = argparse.ArgumentParser(
@@ -41,30 +45,34 @@ def main(argv=None):
     # Only allow one of debug/quiet mode
     verbosity_group = parser.add_mutually_exclusive_group()
     verbosity_group.add_argument("-d", "--debug",
-                                 action='store_true', default=False,
-                                 help="Turn on debugging")
+                                 action='store_const', const=logging.DEBUG,
+                                 dest="output_level", default=logging.INFO,
+                                 help="print debugging")
     verbosity_group.add_argument("-q", "--quiet",
-                                 action="store_true", default=False,
+                                 action="store_const", const=logging.WARNING,
+                                 dest="output_level",
                                  help="run quietly")
+    parser.add_argument("-f", "--log_file",
+                        help="Log output to file", metavar="FILE")
     parser.add_argument("--version", action="version", version="%(prog)s 1.0")
     parser.add_argument('args', metavar='args', type=str, nargs='+',
                         help='some extra arguments' +\
                             ' (use "error" to trigger an error)')
     args = parser.parse_args()
-
-    global output
-    output = print if not args.quiet else lambda s: None
-    global debug
-    debug = print if args.debug else lambda s: None
-
-    output("Processing arguments...")
+    output_handler.setLevel(args.output_level)
+    if args.log_file:
+        file_handler = logging.FileHandler(args.log_file)
+        file_handler.setFormatter(logging.Formatter("%(asctime)s:%(message)s"))
+        output.addHandler(file_handler)
+        output.debug("Logging to file {}".format(args.log_file))
+    output.info("Processing arguments...")
     for arg in args.args:
-        debug("Processing '{}'...".format(arg))
+        output.debug("Processing '{}'...".format(arg))
         error = process(arg)
         if error:
             # Example of using error() method, which exits
             parser.error("Bad argument \"{}\"".format(arg))
-    output("Rest of main() would normally run here...")
+    output.warning("Rest of main() would normally run here...")
     return(0)
 
 if __name__ == "__main__":
