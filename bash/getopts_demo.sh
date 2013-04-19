60785eab385e8178961e0b6028bcb582f1a5ff32
Von Welch <von@vwelch.com>
2013-04-18 20:17:07 -0400

    bash/getopts_demo.sh: added usage, catch missing argument.

--- bash/getopts_demo.sh
+++ bash/getopts_demo.sh
@@ -2,19 +2,36 @@
 #
 # Example of using getopts
 
+usage() {
+    cat <<EOF
+Usage: $0 [<options>]
+EOF
+}
+
 # Leading colon means silent errors, script will handle them
 # Colon after a parameter, means that parameter has an argument
-while getopts ":a" opt; do
+while getopts ":ab:h" opt; do
   case $opt in
     a)
       echo "-a was triggered!" >&2
       ;;
+    b)
+      echo "-b $OPTARG was given." >&2
+      ;;
+    h)
+      usage
+      ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
       ;;
   esac
 done
 
 shift $(($OPTIND - 1))
 
-echo "$@"
+echo "Extra arguments: $@"
+exit 0
