3a4d5130c83256173e3454aec6b3891476c44ad1
Von Welch <von@vwelch.com>
2014-04-07 23:22:11 -0400

    exception-reraise.py: Cleanup, modify exception message

--- python/exception-reraise.py
+++ python/exception-reraise.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-#
-# How to reraise a python exception and keep the original stacktrace.
+"""How to reraise a python exception and keep the original stacktrace
+and a modified message."""
 
 import sys
 
+
 class ExceptionHolder:
-    """Raise an exception hold it and then re-raise it with original stack trace.
+    """Raise an exception, hold it and then re-raise it with original stack
+    trace and a modified modify the message.
 
     Kudos:
     http://nedbatchelder.com/blog/200711/rethrowing_exceptions_in_python.html
@@ -14,17 +16,20 @@ class ExceptionHolder:
     def part_one(self):
         """Create exception but hold it."""
         try:
-            raise Exception("Hello World!")
-        except Exception, ex:
+            raise RuntimeError("Hello World!")
+        except Exception:
             self.exc_info = sys.exc_info()
 
     def part_two(self):
-        """Throw exeception generated in part_one."""
-        # This exception will seem to come from part_one()
-        raise self.exc_info[0], self.exc_info[1], self.exc_info[2]
+        """Throw exeception generated in part_one with modified message.
+
+        This exception will seem to come from part_one()."""
+        exc_class, exc, tb = self.exc_info
+        # Create new exception as original class and modified message
+        new_exc = exc_class("Re-raised exception: %s"
+                            % (exc or exc_class))
+        raise self.exc_info[0], new_exc, self.exc_info[2]
 
 eh = ExceptionHolder()
 eh.part_one()
 eh.part_two()
-
-        
