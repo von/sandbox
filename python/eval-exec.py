6d1ca36ad294d901ff5291daab0ab9e0a0636520
Von Welch <von@vwelch.com>
2014-05-01 13:30:41 -0400

    eval-exec.py: Added demonstration of importing

--- python/eval-exec.py
+++ python/eval-exec.py
@@ -7,3 +7,11 @@ print eval("a")
 # Setting a value needs to be done with exec()
 exec("a=2")
 print eval("a+4")
+
+# Dynamically import modules
+for module in [ "os", "sys" ]:
+    exec("import {}".format(module))
+
+# And show above worked...
+os.chdir("/")  # noqa
+print sys.copyright  # noqa
