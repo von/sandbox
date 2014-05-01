ca44ef50a3a13b14d2c6fb675ecff42422d2a826
Von Welch <von@vwelch.com>
2014-05-01 13:31:32 -0400

    eval-exec.py: pep8 cleanup

--- python/eval-exec.py
+++ python/eval-exec.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Examples of eval()"""
 
-a=4
+a = 4
 print eval("a")
 
 # Setting a value needs to be done with exec()
@@ -9,7 +9,7 @@ exec("a=2")
 print eval("a+4")
 
 # Dynamically import modules
-for module in [ "os", "sys" ]:
+for module in ["os", "sys"]:
     exec("import {}".format(module))
 
 # And show above worked...
