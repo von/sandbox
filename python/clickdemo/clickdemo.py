c657856a4ea159fc31101c2de6a92241509aefc6
Von Welch <von@vwelch.com>
2015-09-17 14:28:15 -0400

    clickdemo.py: Added default command and 'hello' subcommand

--- python/clickdemo/clickdemo.py
+++ python/clickdemo/clickdemo.py
@@ -1,7 +1,18 @@
 import click
 
 
-@click.command()
-def cli():
-    """Example script."""
-    click.echo('Hello World!')
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo('I was invoked without subcommand')
+    else:
+        click.echo('I am about to invoke %s' % ctx.invoked_subcommand)
+
+
+@cli.command()
+@click.option('--count', default=1, help='number of greetings')
+@click.argument('name')
+def hello(count, name):
+    for x in range(count):
+        click.echo('Hello %s!' % name)
