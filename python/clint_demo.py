80b8aaf83d06f4a661dca79a60997a004649c585
Von Welch <von@vwelch.com>
2015-09-17 11:22:08 -0400

    clint_demo.py: whitespace cleanup

--- python/clint_demo.py
+++ python/clint_demo.py
@@ -13,5 +13,5 @@ with indent(4, quote='>>>'):
     puts(colored.red('Files detected: ') + str(args.files))
     puts(colored.red('NOT Files detected: ') + str(args.not_files))
     puts(colored.red('Grouped Arguments: ') + str(dict(args.grouped)))
-    
+
 print
