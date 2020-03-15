befa79c79c771f4fad87718949b4f009c7b3d1de
Von Welch <von@vwelch.com>
2020-03-15 14:56:04 -0400

    string-format.py: added examples of dynamic formatting

--- python/string-format.py
+++ python/string-format.py
@@ -36,15 +36,41 @@ b = B("World")
 print "str: {0!s}\nrepr: {0!r}".format(b)
 
 class C:
+    def __init__(self):
+        self.i = 0
+
     """When passed to format() just returns format_spec capitalized"""
     def __format__(self, format_spec):
         """http://www.python.org/dev/peps/pep-3101/"""
         return string.capwords(format_spec)
 
-c = C()
+    @property
+    def dynamic(self):
+        """Return an incrementing number"""
+        self.i += 1
+        return self.i
 
+c = C()
 print "c: {:hello world}".format(c)
+print "c: {c.dynamic} {c.dynamic} {c.dynamic}".format(c=c)
 
 # Formatting of named placeholders
 # Take i, right align (>), and pad with 0  to length 2
-print "{i:0>2}".format(i=3)
+print "{c.dynamic:0>2}".format(c=c)
+
+class D:
+    """Run the format_spec as python code
+
+    WARNING: Not to be used with untrusted format_specs
+
+    This may not be a good idea, just exploring what is possible.
+
+    Note that this won't handle multi-command/multi-line code as
+    that requires exec() and complexity to get the output.
+    See https://stackoverflow.com/a/52361938/197789"""
+    def __format__(self, format_spec):
+        """http://www.python.org/dev/peps/pep-3101/"""
+        return str(eval(format_spec))
+
+d = D()
+print "d: {d:4+5}".format(d=d)
