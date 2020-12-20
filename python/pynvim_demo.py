02e1992b9eb58b32f1efdd3c86b21ee0c9e5d60e
Von Welch <von@vwelch.com>
2020-12-20 11:00:48 -0500

    pynvim_demo.py: enhance demo

--- python/pynvim_demo.py
+++ python/pynvim_demo.py
@@ -4,22 +4,39 @@ import sys
 
 from pynvim import attach
 
-# Create a python API session attached to nvim started as follows:
-#   NVIM_LISTEN_ADDRESS=/tmp/nvim nvim
+# Create a python API session attached
 try:
     nvim = attach('socket', path='/tmp/nvim')
 except FileNotFoundError:
     print("Could not connect to nvim.")
     sys.exit(1)
 
-# Now do some work.
-buffer = nvim.current.buffer  # Get the current buffer
-buffer[0] = 'replace first line'
-buffer[:] = ['replace whole buffer']
+# Find or Create the demo buffer
+demobufnum = nvim.eval('bufadd("pynvim_demo_buffer")')
+print(f"Demo buffer is buffer #{demobufnum}")
 
-nvim.command('split')
+print("Buffers:")
+for buffer in nvim.buffers:
+    if buffer.valid:
+        print(f"{buffer.number}:{buffer.name}")
+    if buffer.number == demobufnum:
+        demobuf = buffer
 
-nvim.windows[1].height = 10
+print("Windows:")
+for window in nvim.windows:
+    print(f"  Buffer name: {window.buffer.name}"
+          f" ({window.width}x{window.height})")
+
+# Switch to demo buffer and do some work
+nvim.command(f'buffer {demobufnum}')
+count = int(demobuf[0])+1 if demobuf[0].isnumeric() else 1
+# Count of number of times we've run in line 1
+demobuf[0] = str(count)
+# And append a new line...
+demobuf.append('Hello world! This is the demo buffer!')
+
+# nvim.command('split')
+# nvim.windows[1].height = 10
 
 nvim.vars['global_var'] = [1, 2, 3]
 vars = nvim.eval('g:global_var')
