71667006305198a890a5b86a81c9c5d508ebdab5
Von Welch <von@vwelch.com>
2022-03-03 20:52:45 -0500

    fzf-file-browser.sh: use ctrl characters in bindings

--- fzf/fzf-file-browser.sh
+++ fzf/fzf-file-browser.sh
@@ -5,6 +5,6 @@ find * | \
       --height=50% --layout=reverse --info=inline --border --margin=1 --padding=1 \
       --preview="ls -l {}" --preview-window=down,3,wrap \
       --bind "ctrl-m:execute:less {}" \
-      --bind "e:execute:view {}" \
-      --bind "p:toggle-preview" \
-      --bind "q:abort"
+      --bind "ctrl-e:execute:view {}" \
+      --bind "ctrl-p:toggle-preview" \
+      --bind "ctrl-q:abort"
