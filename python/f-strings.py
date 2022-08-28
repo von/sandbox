ed1ab370620113cfa5090f0046080d928ed99be0
Von Welch <von@vwelch.com>
2022-08-28 15:54:58 -0400

    f-strings.py: added examples of counting functions

--- python/f-strings.py
+++ python/f-strings.py
@@ -12,3 +12,35 @@ print("They can also include expressions:")
 a=5
 b=10
 print(f"{a} + {b} = {a + b}")
+
+
+# One way to count, with a class
+class Counter:
+    def __init__(self):
+        self.v = 1
+
+    def __call__(self):
+        r = self.v
+        self.v += 1
+        return r
+
+
+counter = Counter()
+print(f"{counter()} {counter()} {counter()}")
+
+
+# Another way to count, with a closure
+# Kudos: https://stackoverflow.com/a/1261952/197789
+def make_counter(start=1):
+    count = start
+
+    def counter():
+        nonlocal count
+        r = count
+        count += 1
+        return r
+    return counter
+
+
+counter = make_counter(4)
+print(f"{counter()} {counter()} {counter()}")
