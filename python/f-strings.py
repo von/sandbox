a8284c70c7de1bfa33dbaaa9d571fdd891c7e6a3
Von Welch <von@vwelch.com>
2022-08-29 09:44:37 -0400

    f-strings.py: PEP8 whitespace fixes

--- python/f-strings.py
+++ python/f-strings.py
@@ -5,12 +5,12 @@ https://www.python.org/dev/peps/pep-0498/
 """
 
 print("A python f-string renames parameters with coorsponding variable names.")
-name="world"
+name = "world"
 print(f"Hello {name}!")
 
 print("They can also include expressions:")
-a=5
-b=10
+a = 5
+b = 10
 print(f"{a} + {b} = {a + b}")
 
 
