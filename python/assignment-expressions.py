0a8c9ad219947e297feb67a5fb07ec10886e2424
Von Welch <von@vwelch.com>
2022-12-11 10:43:51 -0500

    assignment-expressions.py: added elif and regex examples

--- python/assignment-expressions.py
+++ python/assignment-expressions.py
@@ -2,6 +2,7 @@
 """Demonstrate PEP 572 assignment expressions"""
 # Kudos: https://stackoverflow.com/a/55882141/197789
 
+import re
 import sys
 
 a = 4
@@ -16,5 +17,25 @@ print(f"a = {a}")
 # Should set a to 8 and evaluate as True
 if a := 8:
     print(f"a = {a}")
+
+# Demonstrate if/elif context
+# Put in parens to force desired ordering. Otherwise "10 > 20" evaluates
+# first and a will be set to False
+if (a := 10) > 20:
+    print(f"{a} > 20 - should not get here!")
+elif a < 12:
+    print(f"{a} < 12")
+else:
+    print(f"!{a} < 12 - should not get here!")
+
+# Example with regexs
+r1 = re.compile("abc*")
+r2 = re.compile("def*")
+line = "deffffffff!"
+
+if m := r1.match(line):
+    print(f"{line} matched r1 - should not get here!")
+elif m := r2.match(line):
+    print(f"{line} matched r2 as expected: {m.group(0)}")
 
 sys.exit(0)
