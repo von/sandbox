53fde1d5ebd0a04ff30a7c9586ad22c8b87a40d0
Von Welch <von@vwelch.com>
2023-04-18 09:49:57 -0400

    bash/argument-options.sh: use array for options to perserve whitespace

--- bash/argument-options.sh
+++ bash/argument-options.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-# Kudos: https://stackoverflow.com/a/33271194/197789
+# Demonstrate how to slice commend-line options into arrays.
+# Kudos:
+# https://stackoverflow.com/a/33271194/197789
+# https://stackoverflow.com/a/1215592/197789
 
-options=${*%${!#}} # all parameters except the last
-echo "Options: ${options}"
+# Everything but last parameter is an option.
+# Use arrays to preserves whitespace in options
+declare -a options
+options=( ${@:1:$#-1} ) # all parameters except the last
+echo "Options: ${options[@]@Q}"
 
+# Last parameter is special.
 last=${@:$#} # last parameter
 echo "Last parameter: ${last}"
 
